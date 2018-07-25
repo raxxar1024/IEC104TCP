373a44cccf2ddde2d311e1f9102af47652d5c490
raxxar1024 <yjw013809@gmail.com>
2018-07-25 09:18:48 +0800

    reformat

--- example.py
+++ example.py
@@ -4,9 +4,4 @@ from iec104_tcp_packets import *
 server_ip = '123.1.1.99'
 client = iec104_tcp_client(server_ip)
 for p in plist:
-    print client.sendOne(p)
-	
-
-
-
-
+    print(client.sendOne(p))
