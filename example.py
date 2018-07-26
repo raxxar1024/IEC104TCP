659eabe99a86bcdf54cae9adc949055b3c99c331
raxxar1024 <yjw013809@gmail.com>
2018-07-26 09:54:44 +0800

    optimize

--- example.py
+++ example.py
@@ -1,5 +1,5 @@
-from iec104client import *
-from iec104_tcp_packets import *
+from iec104client import iec104_tcp_client
+from iec104_tcp_packets import plist
 
 server_ip = '123.1.1.99'
 client = iec104_tcp_client(server_ip)
